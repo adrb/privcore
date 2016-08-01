dd2b0c55b35ed38490acb89e5b18c90def56bada
adrb <adrb@privcore.net>
2016-08-01 15:20:49 +0200

    privcore logo for owncloud

---
+++ ansible/roles/owncloud/files/privcore_owncloud_theme/settings/personal.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* This is an example template that overrides the default one.
+* Provided FREE of copyright, support and generally disowned by the
+* creator. Use at your own peril.
+*/
+
+// No idea what you'd put here, to be honest
+
+// EOF
