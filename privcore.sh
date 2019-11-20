7ea05beecd67a3c587a7b6c1ed69e17ea307a4e9
adrb <adrian.brzezinski@adrb.pl>
2019-11-20 15:41:44 +0100

    Use ansible mitogen which vastly improve ansible prefromance

--- privcore.sh
+++ privcore.sh
@@ -4,6 +4,8 @@
 # license: https://creativecommons.org/licenses/by-nc-nd/3.0/
 #
 
+set -e
+
 if [ "$(id -u)" -ne 0 ] ; then
   echo "You must be root to run this program!"
   exit 1
@@ -13,7 +15,7 @@ fi
 # get Debian's codename
 if ( ! dpkg -l lsb-release >/dev/null 2>&1 ) ; then
   echo "Installing lsb-release..."
-  apt-get --yes install lsb-release >/dev/null || exit 1
+  apt-get --yes install lsb-release >/dev/null
 fi
 
 CODENAME=$(lsb_release --codename --short)
@@ -32,7 +34,7 @@ deb-src http://httpredir.debian.org/debian ${CODENAME}-backports main contrib no
 EOF
 
   echo "Updating indexes of available packages..."
-  apt-get update >/dev/null || exit 1
+  apt-get update >/dev/null
 fi
 
 _install_pkgs=''
@@ -43,12 +45,23 @@ for pkt in ansible python python-dnspython python3-dnspython acl ; do
 done
 if [ -n "$_install_pkgs" ]; then
   echo "Installing: $_install_pkgs..."
-  apt-get --yes install $_install_pkgs >/dev/null || exit 1
+  apt-get --yes install $_install_pkgs >/dev/null
 fi
 
 #
-# run setup
+# run ansible playbooks
 cd $(dirname $0)/ansible
+
+# install ansible plugins
+if [ ! -d plugins ] ; then
+  echo "Installing ansible plugins..."
+  mkdir plugins
+  pushd plugins
+  wget -q https://github.com/dw/mitogen/archive/stable.zip -O mitogen-stable.zip
+  unzip -q mitogen-stable.zip
+  popd
+fi
+
 export ANSIBLE_CONFIG='./ansible.cfg'
 ansible-playbook privcore.yml $*
 
